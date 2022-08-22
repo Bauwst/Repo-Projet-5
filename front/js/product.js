2a844dfec2e4a79da75a93497e71b490bdcddca8
Bauwst <bauwst@gmail.com>
2022-08-22 23:55:34 +0200

    scipt.js 2.0 et product.js (workinprogress)

--- front/js/product.js
+++ front/js/product.js
@@ -1,40 +1,81 @@
 
 const params = new URLSearchParams(document.location.search);
 
-const id = params.get("_id");
+const id = params.get("id");
 
-fetch("http://localhost:3000/api/products")
-    .then(function(res){
-        if(res.ok){
+fetch("http://localhost:3000/api/products/" + id)
+    .then((res) => {
             return res.json();
-        }
     })
     .then(function(productlist) {
-        products(productlist);
+        displayProduct(productlist);
     })
     .catch(function(error) {
-        document.querySelector(".titles").innerHTML = "<p> Erreur </p>";
+        document.querySelector(".titles").innerText = "Erreur";
         console.log(error);
     })
 
 
-function products(item) {
+let itemClient = {};
+itemClient._id = id;
+
+
+function displayProduct(item) {
     let titre = document.querySelector("#title");
     let prix = document.querySelector("#price");
     let description = document.querySelector("#description");
     let couleur = document.querySelector("#colors");
-    let image = document.querySelector("article div.item__img");
+    let image = document.querySelector(".item__img");
 
-    for (let choise of item ){
-        if(id == choise._id){
-            titre.innerHTML = `${choise.name}`;
-            prix.innerHTML = `${choise.price}`;
-            description.innerHTML = `${choise.description}`;
-            image.innerHTML =`<img src="${choise.imageUrl}" alt="${choise.altTxt}">`;
+        if(id == item._id){
+            titre.innerText = item.name;
+            prix.innerText = item.price;
+            description.innerText = item.description;
+            let img = document.createElement('img');
+            img.src = item.imageUrl;
+            img.alt = item.altTxt;
+            image.appendChild(img);
 
-            for (let color of choise.colors){
-                couleur.innerHTML = `<option value="${color}">${color}</option>`;
-            }
-        }
+        for (let color of item.colors){
+            let option = document.createElement('option');
+            option.value = color;
+            option.textContent = color;
+            couleur.appendChild(option);
+        }       
     }
 }
+
+
+let choiseColor = document.querySelector("#colors");
+let colorItem;
+
+choiseColor.addEventListener("input", (eventColor) => {
+    colorItem = eventColor.target.value;
+    itemClient.color = colorItem;
+    console.log(colorItem);
+})
+
+
+let choiseQuantity = document.querySelector('input[id="quantity"]');
+let quantityItem;
+
+choiseQuantity.addEventListener("input", (eventQuantity) => {
+    quantityItem = eventQuantity.target.value;
+    itemClient.quantity = quantityItem;
+    console.log(quantityItem);
+})
+
+
+function checkItem(color, quantity){
+    if(color == null || (quantity < 1 || quantity > 100 ))
+        alert("Veuillez selectionner une couleur et une quantité entre 1 et 100."); 
+}
+
+let cartButton = document.querySelector("#addToCart");
+
+cartButton.addEventListener("click", () => {
+    console.log(colorItem, quantityItem);
+    checkItem(colorItem, quantityItem);
+})
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -5,24 +5,38 @@ fetch("http://localhost:3000/api/products")
         }
     })
     .then(function(productlist) {
-        Canapes(productlist);
+        displayProducts(productlist);
         console.table(productlist);
     })
     .catch(function(error) {
-        document.querySelector(".titles").innerHTML = "<p> Erreur </p>";
+        document.querySelector(".titles").innerText = "Erreur";
         console.log(error);
     })
 
-function Canapes(index) {
+
+function displayProducts(articles) {
     let itemzone = document.querySelector("#items");
-    for (let article of index){
-        itemzone.innerHTML += `<a href="./product.html?_id=${article._id}">
-        <article>
-          <img src="${article.imageUrl}" alt="${article.altTxt}">
-          <h3 class="productName">${article.name}</h3>
-          <p class="productDescription">${article.description}</p>
-        </article>
-      </a>`;
+    for (let element of articles){
+        let a = document.createElement('a');
+        a.href = './product.html?id=' + element._id;
+        let article = document.createElement('article');
+        let img = document.createElement('img');
+        img.src = element.imageUrl;
+        img.alt = element.altTxt;
+        let h3 = document.createElement("h3");
+        h3.className = 'productName';
+        h3.textContent = element.name;
+        let p = document.createElement("p");
+        p.className = 'productDescription';
+        p.textContent = element.description;
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
+        a.appendChild(article);
+        itemzone.appendChild(a);
     }
-    } 
+} 
+
+
+
 
